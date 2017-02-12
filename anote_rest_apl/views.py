6d2240b52809e094a582daa11d0c9e4a276ca9d2
akbarpn136 <akbarpn136@gmail.com>
2017-02-12 16:28:47 +0700

    membuat class untuk crud kegiatan dan catatan

--- anote_rest_apl/views.py
+++ anote_rest_apl/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+
+from . import serializers
+from . import models
+
 
 # Create your views here.
+class DaftarKegiatan(generics.ListCreateAPIView):
+    queryset = models.Kegiatan.objects.all()
+    serializer_class = serializers.KegiatanSerializer
+
+
+class ModifikasiKegiatan(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Kegiatan.objects.all()
+    serializer_class = serializers.KegiatanSerializer
+
+    def get_object(self):
+        q = get_object_or_404(models.Kegiatan, pk=self.kwargs['kegiatan_id'])
+
+        return q
+
+
+class DaftarCatatan(generics.ListCreateAPIView):
+    queryset = models.Catatan.objects.all()
+    serializer_class = serializers.CatatanSerializer
+
+    def get_queryset(self):
+        q = models.Catatan.objects.filter(kegiatan=self.kwargs['kegiatan_id'])
+
+        return q
+
+
+class ModifikasiCatatan(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Catatan.objects.all()
+    serializer_class = serializers.CatatanSerializer
+
+    def get_queryset(self):
+        q = models.Catatan.objects.filter(kegiatan=self.kwargs['kegiatan_id'])
+
+        return q
+
+    def get_object(self):
+        q = get_object_or_404(models.Catatan,
+                              kegiatan=self.get_queryset(),
+                              pk=self.kwargs['catatan_id'])
+
+        return q
