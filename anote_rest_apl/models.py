454c8c64e5d16f736d4f0e5bd8efae1e294b2ec1
akbarpn136 <akbarpn136@gmail.com>
2017-02-12 11:46:48 +0700

    membuat model data kegiatan dan catatan beserta migrasi data sebagai version control database

--- anote_rest_apl/models.py
+++ anote_rest_apl/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Kegiatan(models.Model):
+    nama = models.CharField(max_length=255)
+    stkk = models.CharField(max_length=255)
+    kode = models.CharField(max_length=100)
+    tanggal = models.DateField()
+
+    class Meta:
+        verbose_name_plural = 'Kegiatan'
+
+    def __str__(self):
+        return '%s: %s' % (self.stkk, self.nama)
+
+
+class Catatan(models.Model):
+    OPSI_JENIS = (
+        ('LK', 'Lembar Kerja'),
+        ('LB', 'Logbook'),
+    )
+
+    jenis = models.CharField(max_length=3, choices=OPSI_JENIS, default='LK')
+    kegiatan = models.ForeignKey(Kegiatan, on_delete=models.CASCADE, related_name='nama_kegiatan')
+    nomor = models.CharField(max_length=255)
+    referensi = models.CharField(max_length=255)
+    tanggal = models.DateField()
+    butir = models.CharField(max_length=120)
+    angka = models.FloatField()
+    dibuat = models.ForeignKey('auth.User', related_name='dibuat_oleh')
+    diketahui = models.ForeignKey('auth.User', related_name='diketahui_oleh')
