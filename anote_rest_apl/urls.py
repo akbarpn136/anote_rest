1d94fd59e6e1f427ceff99a446808b0a79b45360
akbarpn136 <akbarpn136@gmail.com>
2017-02-13 21:38:27 +0700

    menambahkan route include untuk anggota dari kegiatan

--- anote_rest_apl/urls.py
+++ anote_rest_apl/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.DaftarKegiatan.as_view(), name='daftar_kegiatan'),
     url(r'^(?P<kegiatan_id>\d+)$', views.ModifikasiKegiatan.as_view(), name='modif_kegiatan'),
     url(r'^(?P<kegiatan_id>\d+)/catatan/$', views.DaftarCatatan.as_view(), name='daftar_catatan'),
+    url(r'^(?P<kegiatan_id>\d+)/anggota/', include('anote_rest_member.urls', namespace='anggota_kegiatan')),
     url(r'^(?P<kegiatan_id>\d+)/catatan/(?P<catatan_id>\d+)/$', views.DaftarCatatan.as_view(),
         name='daftar_catatan'),
     url(r'^(?P<kegiatan_id>\d+)/member/$', views.DaftarCatatan.as_view(), name='daftar_member'),
