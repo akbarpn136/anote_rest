48faea071c01063e439f12569ca8e298ee99338a
akbarpn136 <akbarpn136@gmail.com>
2017-02-13 21:38:50 +0700

    menambahkan route untuk anggota dari kegiatan

--- anote_rest_member/urls.py
+++ anote_rest_member/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.DaftarKegiatan.as_view(), name='daftar_kegiatan'),
+    url(r'^$', views.DaftarMemberKegiatan.as_view(), name='daftar_member'),
 ]
