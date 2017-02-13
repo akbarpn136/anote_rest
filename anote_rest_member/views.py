91e0edb2c4a622cb23f57fb0c562f439cc8bc52e
akbarpn136 <akbarpn136@gmail.com>
2017-02-13 21:39:17 +0700

    membuat class untuk list dan tambah anggota dalam kegiatan

--- anote_rest_member/views.py
+++ anote_rest_member/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics
 
+from . import models
+from . import serializers
+from anote_rest_butir.views import IsSuperUser
+
 
 # Create your views here.
 class DaftarMemberKegiatan(generics.ListCreateAPIView):
-    pass
+    queryset = models.MemberKegiatan.objects.all()
+    serializer_class = serializers.MemberSerializer
+    permission_classes = (IsSuperUser,)
