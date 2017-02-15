5429229e7c912b6ea14f4d4caeff2d1e4bcc3398
akbarpn136 <akbarpn136@gmail.com>
2017-02-15 10:06:41 +0700

    tambah class baru untuk modifikasi keanggotaan suatu kegiatan

--- anote_rest_member/views.py
+++ anote_rest_member/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from . import models
 from . import serializers
 from anote_rest_butir.views import IsSuperUser
+from anote_rest_apl.models import Kegiatan
 
 
 # Create your views here.
@@ -10,3 +12,24 @@ class DaftarMemberKegiatan(generics.ListCreateAPIView):
     queryset = models.MemberKegiatan.objects.all()
     serializer_class = serializers.MemberSerializer
     permission_classes = (IsSuperUser,)
+
+    def perform_create(self, serializer):
+        keg = get_object_or_404(Kegiatan, pk=self.kwargs['kegiatan_id'])
+
+        serializer.save(kegiatan=keg)
+
+
+class ModifikasiMemberKegiatan(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.MemberKegiatan.objects.all()
+    serializer_class = serializers.MemberSerializer
+    permission_classes = (IsSuperUser,)
+
+    def get_object(self):
+        q = get_object_or_404(models.MemberKegiatan, pk=self.kwargs['member_id'])
+
+        return q
+
+    def perform_update(self, serializer):
+        keg = get_object_or_404(Kegiatan, pk=self.kwargs['kegiatan_id'])
+
+        serializer.save(kegiatan=keg)
