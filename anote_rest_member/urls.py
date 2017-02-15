a9cc5758d85efacac972cf1d590cfc635d81b2fd
akbarpn136 <akbarpn136@gmail.com>
2017-02-15 10:06:18 +0700

    tambah route baru untuk modifikasi keanggotaan suatu kegiatan

--- anote_rest_member/urls.py
+++ anote_rest_member/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.DaftarMemberKegiatan.as_view(), name='daftar_member'),
+    url(r'^(?P<member_id>\d+)/$', views.ModifikasiMemberKegiatan.as_view(), name='modifikasi_member'),
 ]
