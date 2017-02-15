9008fd3aa51302c59a214c2fdda25a49a1f02a56
akbarpn136 <akbarpn136@gmail.com>
2017-02-15 22:31:41 +0700

    menambahkan custom directive untuk interaksi dropdown dan collapse navbar

--- static/anotePage1/src/app/app.module.ts
+++ static/anotePage1/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MunculiniDirective } from './an-directive/munculini.directive';
+import { MunculituDirective } from './an-directive/munculitu.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MunculiniDirective,
+    MunculituDirective
   ],
   imports: [
     BrowserModule,
