bb006b17c3ed7dcbb60e478c32d096708e5621c8
akbarpn136 <akbarpn136@gmail.com>
2017-02-17 08:35:44 +0700

    menambahkan komponen untuk daftar catatan

---
+++ static/anotePage1/src/app/kegiatan-catatan/daftar-catatan/daftar-catatan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'an-daftar-catatan',
+  templateUrl: './daftar-catatan.component.html',
+  styleUrls: ['./daftar-catatan.component.css']
+})
+export class DaftarCatatanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
