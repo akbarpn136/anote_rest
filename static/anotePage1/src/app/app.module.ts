196608efe3215bbb7cff6abafac8a138ed48ba81
akbarpn136 <akbarpn136@gmail.com>
2017-02-17 08:33:42 +0700

    menambahkan beberapa komponen aplikasi

--- static/anotePage1/src/app/app.module.ts
+++ static/anotePage1/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MunculiniDirective } from './an-directive/munculini.directive';
 import { MunculituDirective } from './an-directive/munculitu.directive';
+import { KegiatanCatatanComponent } from './kegiatan-catatan/kegiatan-catatan.component';
+import { DaftarKegiatanComponent } from './kegiatan-catatan/daftar-kegiatan/daftar-kegiatan.component';
+import { DaftarCatatanComponent } from './kegiatan-catatan/daftar-catatan/daftar-catatan.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MunculiniDirective,
-    MunculituDirective
+    MunculituDirective,
+    KegiatanCatatanComponent,
+    DaftarKegiatanComponent,
+    DaftarCatatanComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
