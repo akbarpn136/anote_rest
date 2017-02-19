cc209797f919ea6a1f8c55b0388fd335dd4705d9
akbarpn136 <akbarpn136@gmail.com>
2017-02-20 05:37:24 +0700

    menambahkan fungsionalitas pagination dan tambah tombol tambah kegiatan baru

--- static/anotePage1/src/app/pagination/pagination.component.ts
+++ static/anotePage1/src/app/pagination/pagination.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
-  selector: 'an-pagination',
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.css']
+    selector: 'an-pagination',
+    templateUrl: './pagination.component.html',
+    styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
+    @Input() hasNext:boolean;
+    @Input() hasPrev:boolean;
+    @Input() total:number;
+    @Input() range:number;
+    @Output() offset = new EventEmitter<number>();
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onNext(event) {
+        event.preventDefault();
+        this.offset.emit(this.range);
+    }
+    onPrev(event) {
+        event.preventDefault();
+        this.offset.emit(-this.range);
+    }
 }
