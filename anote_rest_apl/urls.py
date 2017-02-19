ba80ff23540d2f381b7d34b3545e3807b5a37b7c
akbarpn136 <akbarpn136@gmail.com>
2017-02-20 05:38:52 +0700

    menambahkan route untuk mendapatkan token

--- anote_rest_apl/urls.py
+++ anote_rest_apl/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.DaftarKegiatan.as_view(), name='daftar_kegiatan'),
-    url(r'^(?P<kegiatan_id>\d+)$', views.ModifikasiKegiatan.as_view(), name='modif_kegiatan'),
+    url(r'^token-auth/$', obtain_auth_token),
+    url(r'^(?P<kegiatan_id>\d+)$', views.ModifikasiKegiatan.as_view(), name='modifikasi_kegiatan'),
     url(r'^(?P<kegiatan_id>\d+)/catatan/$', views.DaftarCatatan.as_view(), name='daftar_catatan'),
     url(r'^(?P<kegiatan_id>\d+)/anggota/', include('anote_rest_member.urls', namespace='anggota_kegiatan')),
-    url(r'^(?P<kegiatan_id>\d+)/catatan/(?P<catatan_id>\d+)/$', views.DaftarCatatan.as_view(),
-        name='daftar_catatan'),
+    url(r'^(?P<kegiatan_id>\d+)/catatan/(?P<catatan_id>\d+)/$', views.ModifikasiCatatan.as_view(),
+        name='modifikasi_catatan'),
     url(r'^(?P<kegiatan_id>\d+)/member/$', views.DaftarCatatan.as_view(), name='daftar_member'),
 ]
