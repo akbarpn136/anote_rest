e450d9d8feb13e679315dc28ec769a36fa71b459
akbarpn136 <akbarpn136@gmail.com>
2017-02-20 15:23:03 +0700

    menambahkan modul route dan login component

--- static/anotePage1/src/app/app.module.ts
+++ static/anotePage1/src/app/app.module.ts
@@ -1,34 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MunculiniDirective } from './an-directive/munculini.directive';
-import { MunculituDirective } from './an-directive/munculitu.directive';
-import { KegiatanCatatanComponent } from './kegiatan-catatan/kegiatan-catatan.component';
-import { DaftarKegiatanComponent } from './kegiatan-catatan/daftar-kegiatan/daftar-kegiatan.component';
-import { DaftarCatatanComponent } from './kegiatan-catatan/daftar-catatan/daftar-catatan.component';
-import { PaginationComponent } from './pagination/pagination.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {MunculiniDirective} from './an-directive/munculini.directive';
+import {MunculituDirective} from './an-directive/munculitu.directive';
+import {KegiatanCatatanComponent} from './kegiatan-catatan/kegiatan-catatan.component';
+import {DaftarKegiatanComponent} from './kegiatan-catatan/daftar-kegiatan/daftar-kegiatan.component';
+import {DaftarCatatanComponent} from './kegiatan-catatan/daftar-catatan/daftar-catatan.component';
+import {PaginationComponent} from './pagination/pagination.component';
+import {route} from './app.route';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    MunculiniDirective,
-    MunculituDirective,
-    KegiatanCatatanComponent,
-    DaftarKegiatanComponent,
-    DaftarCatatanComponent,
-    PaginationComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        MunculiniDirective,
+        MunculituDirective,
+        KegiatanCatatanComponent,
+        DaftarKegiatanComponent,
+        DaftarCatatanComponent,
+        PaginationComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        route
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
