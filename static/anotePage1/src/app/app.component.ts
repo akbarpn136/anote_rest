1da8db9f38e85146c33fe88aebb535fe79616905
akbarpn136 <akbarpn136@gmail.com>
2017-02-21 21:50:25 +0700

    menambahkan service guard

--- static/anotePage1/src/app/app.component.ts
+++ static/anotePage1/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {GuardAuthorizeService} from "./services/guard-authorize.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [GuardAuthorizeService]
 })
 export class AppComponent {
-  title = 'aNote';
+    title = 'aNote';
 }
