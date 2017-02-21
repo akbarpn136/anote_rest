32893c324fcc39e0d6b64ade285912b429ea88f9
akbarpn136 <akbarpn136@gmail.com>
2017-02-21 21:51:27 +0700

    menambahkan variabel header dan deskripsi

--- static/anotePage1/src/app/kegiatan-catatan/daftar-catatan/daftar-catatan.component.ts
+++ static/anotePage1/src/app/kegiatan-catatan/daftar-catatan/daftar-catatan.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'an-daftar-catatan',
-  templateUrl: './daftar-catatan.component.html',
-  styleUrls: ['./daftar-catatan.component.css']
+    selector: 'an-daftar-catatan',
+    templateUrl: './daftar-catatan.component.html',
+    styleUrls: ['./daftar-catatan.component.css']
 })
 export class DaftarCatatanComponent implements OnInit {
+    headerText:string;
+    deskripsi:string;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.headerText = 'INFO';
+        this.deskripsi = 'Tekan tautan kegiatan disamping untuk melihat catatan terakhir.';
+    }
 }
