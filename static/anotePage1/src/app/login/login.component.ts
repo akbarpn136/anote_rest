9c2ae2a315e36010d034d893297bd681ab40ef7f
akbarpn136 <akbarpn136@gmail.com>
2017-02-21 21:54:09 +0700

    membuat fungsi interaktif untuk login

--- static/anotePage1/src/app/login/login.component.ts
+++ static/anotePage1/src/app/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthorizeService} from "../services/authorize.service";
 
 @Component({
-  selector: 'an-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'an-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+    providers: [AuthorizeService]
 })
 export class LoginComponent implements OnInit {
+    username: string;
+    password: string;
+    warnStat: boolean = false;
+    message: string = 'Username/password salah...';
 
-  constructor() { }
+    constructor(private auth: AuthorizeService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onAuthSubmit() {
+        this.auth.cobaLogin(this.username, this.password).subscribe(
+            (data) => {
+                if (data.hasOwnProperty('token')) {
+                    this.warnStat = false;
+                }
+
+                else {
+                    this.warnStat = true;
+                    this.message = data.toString();
+                }
+            }
+        );
+    }
 }
