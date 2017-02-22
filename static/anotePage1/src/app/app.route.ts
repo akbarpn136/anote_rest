fcba8c4fb5022d6e055ab1e8276c82dc37fb5f93
akbarpn136 <akbarpn136@gmail.com>
2017-02-23 06:18:25 +0700

    menambahkan service guard kedalam aplikasi route

--- static/anotePage1/src/app/app.route.ts
+++ static/anotePage1/src/app/app.route.ts
@@ -4,9 +4,10 @@
 import {Route, RouterModule} from '@angular/router';
 import {DaftarCatatanComponent} from "./kegiatan-catatan/daftar-catatan/daftar-catatan.component";
 import {LoginComponent} from "./login/login.component";
+import {GuardAuthorizeService} from "./services/guard-authorize.service";
 
 const APP_ROUTES: Route[] = [
-    {path: 'masuk', component: LoginComponent},
+    {path: 'masuk', component: LoginComponent, canActivate:[GuardAuthorizeService]},
     {path: '', component: DaftarCatatanComponent},
     {path: '**', redirectTo:'', pathMatch:'full'},
 ];
