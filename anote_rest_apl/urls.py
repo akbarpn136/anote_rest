9da072f7cfb0fa2b71003a28362a1a028bc330e0
akbarpn136 <akbarpn136@gmail.com>
2017-02-23 11:19:00 +0700

    memperbaiki kesalahan penggunaan / dalam route

--- anote_rest_apl/urls.py
+++ anote_rest_apl/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^$', views.DaftarKegiatan.as_view(), name='daftar_kegiatan'),
     url(r'^token-auth/$', obtain_auth_token),
     url(r'^token-check/(?P<token>\w+)$', views.CheckToken.as_view(), name='cek_token'),
-    url(r'^(?P<kegiatan_id>\d+)$', views.ModifikasiKegiatan.as_view(), name='modifikasi_kegiatan'),
+    url(r'^(?P<kegiatan_id>\d+)/$', views.ModifikasiKegiatan.as_view(), name='modifikasi_kegiatan'),
     url(r'^(?P<kegiatan_id>\d+)/catatan/$', views.DaftarCatatan.as_view(), name='daftar_catatan'),
     url(r'^(?P<kegiatan_id>\d+)/anggota/', include('anote_rest_member.urls', namespace='anggota_kegiatan')),
     url(r'^(?P<kegiatan_id>\d+)/catatan/(?P<catatan_id>\d+)/$', views.ModifikasiCatatan.as_view(),
