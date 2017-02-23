9555c271fe2d30655fdd913bae2c312f5875701b
akbarpn136 <akbarpn136@gmail.com>
2017-02-23 11:12:32 +0700

    mengurangi route pada app

--- static/anotePage1/src/app/app.route.ts
+++ static/anotePage1/src/app/app.route.ts
@@ -2,13 +2,11 @@
  * Created by USER on 2/20/2017.
  */
 import {Route, RouterModule} from '@angular/router';
-import {DaftarCatatanComponent} from "./kegiatan-catatan/daftar-catatan/daftar-catatan.component";
 import {LoginComponent} from "./login/login.component";
 import {GuardAuthorizeService} from "./services/guard-authorize.service";
 
 const APP_ROUTES: Route[] = [
     {path: 'masuk', component: LoginComponent, canActivate:[GuardAuthorizeService]},
-    {path: '', component: DaftarCatatanComponent},
     {path: '**', redirectTo:'', pathMatch:'full'},
 ];
 
