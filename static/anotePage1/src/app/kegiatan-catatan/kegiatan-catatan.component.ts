4961f44f0899a24a983159f194372768a54ea4ec
akbarpn136 <akbarpn136@gmail.com>
2017-02-25 13:56:27 +0700

    memperbaiki penggunaan router-outlet dalam komponen untuk catatan dan kegiatan

--- static/anotePage1/src/app/kegiatan-catatan/kegiatan-catatan.component.ts
+++ static/anotePage1/src/app/kegiatan-catatan/kegiatan-catatan.component.ts
@@ -1,15 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {KegiatanService} from "../services/kegiatan.service";
 import {KegiatanTertentuService} from "../services/kegiatan-tertentu.service";
+import {CatatanKegiatanService} from "../services/catatan-kegiatan.service";
 
 @Component({
     selector: 'an-kegiatan-catatan',
     templateUrl: './kegiatan-catatan.component.html',
     styleUrls: ['./kegiatan-catatan.component.css'],
-    providers: [KegiatanService, KegiatanTertentuService]
+    providers: [KegiatanService, KegiatanTertentuService, CatatanKegiatanService]
 })
 export class KegiatanCatatanComponent implements OnInit {
-    kegiatan_terpilih:any;
+    kegiatan_terpilih: any;
+    catatan_terkait: any;
 
     constructor() {
     }
@@ -19,5 +21,9 @@ export class KegiatanCatatanComponent implements OnInit {
 
     onKegiatan(kegiatan) {
         this.kegiatan_terpilih = kegiatan;
+    }
+
+    onCatatanTerkait(catatan) {
+        this.catatan_terkait = catatan;
     }
 }
