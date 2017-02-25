75b0dbded3b71ec514eed6efef84eed6fb3ad5a2
akbarpn136 <akbarpn136@gmail.com>
2017-02-25 13:57:21 +0700

    memberikan penamaan user setelah berhasil login

--- static/anotePage1/src/app/login/login.component.ts
+++ static/anotePage1/src/app/login/login.component.ts
@@ -1,19 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {AuthorizeService} from "../services/authorize.service";
 import {Router} from "@angular/router";
+import {GuardAuthorizeService} from "../services/guard-authorize.service";
 
 @Component({
     selector: 'an-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
     username: string;
     password: string;
     warnStat: boolean = false;
     message: string = 'Username/password salah...';
+    private sub:any;
 
     constructor(private auth: AuthorizeService,
+                private guard: GuardAuthorizeService,
                 private router: Router) {
     }
 
@@ -21,10 +24,15 @@ export class LoginComponent implements OnInit {
     }
 
     onAuthSubmit() {
-        this.auth.cobaLogin(this.username, this.password).subscribe(
+        this.sub = this.auth.cobaLogin(this.username, this.password).subscribe(
             (data) => {
                 if (data.hasOwnProperty('token')) {
                     this.warnStat = false;
+                    this.guard.checkKeyValid(data['token']).subscribe(
+                        (name) => {
+                            localStorage.setItem('user', name['name']);
+                        }
+                    );
                     //noinspection JSIgnoredPromiseFromCall
                     this.router.navigate(['']);
                 }
@@ -35,5 +43,9 @@ export class LoginComponent implements OnInit {
                 }
             }
         );
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 }
