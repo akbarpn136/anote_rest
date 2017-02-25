e0177f4272ec8122e0499331c5b159417340c962
akbarpn136 <akbarpn136@gmail.com>
2017-02-25 18:14:38 +0700

    menyimpan data user

--- static/anotePage1/src/app/services/catatan-kegiatan.service.ts
+++ static/anotePage1/src/app/services/catatan-kegiatan.service.ts
@@ -1,30 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http, Headers, URLSearchParams, RequestOptions, Response} from "@angular/http";
 import {GuardAuthorizeService} from "./guard-authorize.service";
 
 @Injectable()
 export class CatatanKegiatanService {
     private KEGIATAN_URL: string = 'http://127.0.0.1:8000/api_1/kegiatan/';
-    private qwerty:string;
+    private qwerty: string;
 
-    constructor(private http: Http, private guard:GuardAuthorizeService) {
-        if (!this.guard.canActivate()) {
-            this.qwerty = localStorage.getItem('qwerty');
-        }
-
-        else {
-            this.qwerty = '';
-        }
+    constructor(private http: Http, private guard: GuardAuthorizeService) {
     }
 
     getCatatan(kegiatan_id, offset) {
-        let headers = new Headers({'Content-Type': 'application/json', 'Authorization': `token ${this.qwerty}`});
+        let headers = new Headers({'Content-Type': 'application/json'});
         let params = new URLSearchParams();
 
         // params.set('limit', limit);
         params.set('offset', offset);
 
         let options = new RequestOptions({headers: headers, search: params});
+
+        if (!this.guard.canActivate()) {
+            this.qwerty = localStorage.getItem('qwerty');
+            headers.set('Authorization', `token ${this.qwerty}`);
+        }
+
+        else {
+            this.qwerty = '';
+        }
 
         return this.http.get(`${this.KEGIATAN_URL}${kegiatan_id}/catatan/`, options)
             .map((res: Response) => {
