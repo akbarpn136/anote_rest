c5bc596a7a30c09977764f4a9d1c8e2fda7b4b5d
akbarpn136 <akbarpn136@gmail.com>
2017-02-25 18:21:33 +0700

    memperbaiki penggunaan view dalam route

--- anote_rest_apl/urls.py
+++ anote_rest_apl/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url, include
-from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.DaftarKegiatan.as_view(), name='daftar_kegiatan'),
-    url(r'^token-auth/$', obtain_auth_token),
+    url(r'^token-auth/$', views.GetToken.as_view(), name='ambil_token'),
     url(r'^token-check/(?P<token>\w+)$', views.CheckToken.as_view(), name='cek_token'),
     url(r'^(?P<kegiatan_id>\d+)/$', views.ModifikasiKegiatan.as_view(), name='modifikasi_kegiatan'),
     url(r'^(?P<kegiatan_id>\d+)/catatan/$', views.DaftarCatatan.as_view(), name='daftar_catatan'),
