e04773b2602a4c8fe15ca2b82041a712807f8f2d
akbarpn136 <akbarpn136@gmail.com>
2017-02-25 18:21:09 +0700

    memperbaiki struktur penulisan

--- static/anotePage1/src/app/login/login.component.ts
+++ static/anotePage1/src/app/login/login.component.ts
@@ -1,22 +1,19 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthorizeService} from "../services/authorize.service";
 import {Router} from "@angular/router";
-import {GuardAuthorizeService} from "../services/guard-authorize.service";
 
 @Component({
     selector: 'an-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit, OnDestroy {
+export class LoginComponent implements OnInit {
     username: string;
     password: string;
     warnStat: boolean = false;
     message: string = 'Username/password salah...';
-    private sub:any;
 
     constructor(private auth: AuthorizeService,
-                private guard: GuardAuthorizeService,
                 private router: Router) {
     }
 
@@ -24,17 +21,12 @@ export class LoginComponent implements OnInit, OnDestroy {
     }
 
     onAuthSubmit() {
-        this.sub = this.auth.cobaLogin(this.username, this.password).subscribe(
+        this.auth.cobaLogin(this.username, this.password).subscribe(
             (data) => {
                 if (data.hasOwnProperty('token')) {
                     this.warnStat = false;
-                    this.guard.checkKeyValid(data['token']).subscribe(
-                        (name) => {
-                            localStorage.setItem('user', name['name']);
-                        }
-                    );
                     //noinspection JSIgnoredPromiseFromCall
-                    this.router.navigate(['']);
+                    this.router.navigate(['kegiatan']);
                 }
 
                 else {
@@ -43,9 +35,5 @@ export class LoginComponent implements OnInit, OnDestroy {
                 }
             }
         );
-    }
-
-    ngOnDestroy() {
-        this.sub.unsubscribe();
     }
 }
