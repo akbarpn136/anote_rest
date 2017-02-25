1222d18d1c0f071f92eb67fb285c353a9f3189c0
akbarpn136 <akbarpn136@gmail.com>
2017-02-25 22:14:01 +0700

    menerapkan filter kegiatan untuk masing-masing tingkatan pengguna

--- static/anotePage1/src/app/services/kegiatan.service.ts
+++ static/anotePage1/src/app/services/kegiatan.service.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {Http, Response, Headers, RequestOptions, URLSearchParams} from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import {GuardAuthorizeService} from "./guard-authorize.service";
 
 @Injectable()
 export class KegiatanService {
     private KEGIATAN_URL: string = 'http://127.0.0.1:8000/api_1/kegiatan/';
 
-    constructor(private http: Http) {
+    constructor(private http: Http,
+                private guard: GuardAuthorizeService) {
     }
 
     getKegiatan(offset) {
@@ -16,6 +18,10 @@ export class KegiatanService {
 
         // params.set('limit', limit);
         params.set('offset', offset);
+
+        if (!this.guard.canActivate()) {
+            headers.set('Authorization', `token ${localStorage.getItem('qwerty')}`)
+        }
 
         let options = new RequestOptions({headers: headers, search: params});
 
