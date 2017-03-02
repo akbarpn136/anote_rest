b51106c7aa1b2d102da3b34002ac233df90a7464
akbarpn136 <akbarpn136@gmail.com>
2017-03-02 10:47:49 +0700

    menambahkan fungsi untuk tambah kegiatan

--- static/anotePage1/src/app/kegiatan-catatan/daftar-kegiatan/tambah-kegiatan.component.ts
+++ static/anotePage1/src/app/kegiatan-catatan/daftar-kegiatan/tambah-kegiatan.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {KegiatanService} from "../../services/kegiatan.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'an-tambah-kegiatan',
@@ -9,12 +11,15 @@ export class TambahKegiatanComponent implements OnInit {
     @Input() isShow: boolean = false;
     @Output() isModalShow = new EventEmitter<boolean>();
 
-    private nama:string;
-    private stkk:string;
-    private kode:string;
-    private tanggal:string;
+    private nama: string;
+    private stkk: string;
+    private kode: string;
+    private tanggal: string;
+    private warnStat: boolean;
+    private message: any;
+    private key: any;
 
-    constructor() {
+    constructor(private kegiatanService: KegiatanService, private router: Router) {
     }
 
     ngOnInit() {
@@ -23,5 +28,31 @@ export class TambahKegiatanComponent implements OnInit {
     onModalClose() {
         this.isShow = false;
         this.isModalShow.emit(false);
+    }
+
+    onTambahKegiatanSubmit() {
+        let data = {
+            'nama': this.nama,
+            'stkk': this.stkk,
+            'kode': this.kode,
+            'tanggal': this.tanggal,
+        };
+
+        this.kegiatanService.tambahKegiatan(data).subscribe(
+            () => {
+                this.onModalClose();
+                //noinspection JSIgnoredPromiseFromCall
+                this.router.navigate(['']);
+            },
+            (err) => {
+                this.warnStat = true;
+                this.key = Object.keys(err);
+                this.message = err;
+
+                this.nama = this.key.indexOf('nama') !== -1 ? '' : this.nama;
+                this.stkk = this.key.indexOf('stkk') !== -1 ? '' : this.stkk;
+                this.kode = this.key.indexOf('kode') !== -1 ? '' : this.kode;
+            }
+        );
     }
 }
