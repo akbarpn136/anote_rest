0de3454af3793dfbc01cad2fb99fd934fbda4cf4
akbarpn136 <akbarpn136@gmail.com>
2017-03-02 21:49:21 +0700

    menambahkan route children untuk modifikasi kegiatan

--- static/anotePage1/src/app/app.route.ts
+++ static/anotePage1/src/app/app.route.ts
@@ -6,10 +6,12 @@ import {LoginComponent} from "./login/login.component";
 import {GuardAuthorizeService} from "./services/guard-authorize.service";
 import {DaftarCatatanComponent} from "./kegiatan-catatan/daftar-catatan/daftar-catatan.component";
 import {CatatanComponent} from "./kegiatan-catatan/daftar-catatan/catatan.component";
+import {ModifKegiatanComponent} from "./kegiatan-catatan/daftar-kegiatan/modif-kegiatan.component";
 
 const APP_ROUTES: Route[] = [
     {path: 'masuk', component: LoginComponent, canActivate:[GuardAuthorizeService]},
     {path: 'kegiatan', component: CatatanComponent, children: [
+        {path: ':id/modifikasi', component: ModifKegiatanComponent},
         {path: ':id', component: DaftarCatatanComponent}
     ]},
     {path: '', redirectTo:'kegiatan', pathMatch:'full'},
