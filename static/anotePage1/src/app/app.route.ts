4a53c711c785f5791291629bb7b488a4b3076adb
akbarpn136 <akbarpn136@gmail.com>
2017-03-03 20:10:54 +0700

    tambah route baru untuk akses tautan anggota

--- static/anotePage1/src/app/app.route.ts
+++ static/anotePage1/src/app/app.route.ts
@@ -7,12 +7,15 @@ import {GuardAuthorizeService} from "./services/guard-authorize.service";
 import {DaftarCatatanComponent} from "./kegiatan-catatan/daftar-catatan/daftar-catatan.component";
 import {CatatanComponent} from "./kegiatan-catatan/daftar-catatan/catatan.component";
 import {ModifKegiatanComponent} from "./kegiatan-catatan/daftar-kegiatan/modif-kegiatan.component";
+import {AnggotaKegiatanComponent} from "./kegiatan-catatan/anggota-kegiatan/anggota-kegiatan.component";
 
 const APP_ROUTES: Route[] = [
     {path: 'masuk', component: LoginComponent, canActivate:[GuardAuthorizeService]},
     {path: 'kegiatan', component: CatatanComponent, children: [
         {path: ':id/modifikasi', component: ModifKegiatanComponent},
-        {path: ':id', component: DaftarCatatanComponent}
+        {path: ':id', component: DaftarCatatanComponent, children: [
+            {path: 'anggota', component: AnggotaKegiatanComponent},
+        ]},
     ]},
     {path: '', redirectTo:'kegiatan', pathMatch:'full'},
     {path: '**', redirectTo:'kegiatan', pathMatch:'full'},
