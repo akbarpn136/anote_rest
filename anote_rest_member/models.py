b067d770f9c023d4c7698f7d0319469ce3a0c820
akbarpn136 <akbarpn136@gmail.com>
2017-03-10 11:41:27 +0700

    menambahkan fungsi untuk mengambil nama lengkap beserta kegiatan

--- anote_rest_member/models.py
+++ anote_rest_member/models.py
@@ -25,3 +25,9 @@ class MemberKegiatan(models.Model):
 
     def __str__(self):
         return '%s: %s' % (self.kode_jabatan, self.personil.get_full_name())
+
+    def get_user_fullname(self):
+        return self.personil.get_full_name()
+
+    def get_nama_kegiatan(self):
+        return self.kegiatan.nama
