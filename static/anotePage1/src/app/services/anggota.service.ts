95acdd39f3e4bb00630f307b6591f59520975fa5
akbarpn136 <akbarpn136@gmail.com>
2017-03-10 11:39:15 +0700

    menambahkan service untuk data profil

---
+++ static/anotePage1/src/app/services/anggota.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@angular/core';
+import {Http, RequestOptions, Headers, Response} from "@angular/http";
+import {Observable} from "rxjs";
+
+@Injectable()
+export class AnggotaService {
+    private ANGGOTA_KEGIATAN_URL: string = 'http://localhost:8000/api_1/kegiatan/';
+    private PERSONIL_URL: string = 'http://localhost:8000/api_1/profil/';
+    qwerty: string;
+
+    constructor(private http: Http) {
+    }
+
+    ambilAnggota(kegiatan_id) {
+        this.qwerty = localStorage.getItem('qwerty');
+        let headers = new Headers({'Content-Type': 'application/json'});
+        headers.set('Authorization', `token ${this.qwerty}`);
+
+        let options = new RequestOptions({headers: headers});
+
+        return this.http.get(`${this.ANGGOTA_KEGIATAN_URL}${kegiatan_id}/anggota/`, options)
+            .map((res: Response) => {
+                return res.json();
+            })
+            .catch((err: Response | any) => {
+                if (err instanceof Response) {
+                    return Observable.throw(err.json());
+                }
+
+                else {
+                    return err.message;
+                }
+            });
+    }
+
+    ambilPersonil(personil_id) {
+        this.qwerty = localStorage.getItem('qwerty');
+        let headers = new Headers({'Content-Type': 'application/json'});
+        headers.set('Authorization', `token ${this.qwerty}`);
+
+        let options = new RequestOptions({headers: headers});
+
+        return this.http.get(`${this.PERSONIL_URL}${personil_id}/personil/`, options)
+            .map((res: Response) => {
+                return res.json();
+            })
+            .catch((err: Response | any) => {
+                if (err instanceof Response) {
+                    return Observable.throw(err.json());
+                }
+
+                else {
+                    return err.message;
+                }
+            });
+    }
+}
