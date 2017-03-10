03eda0d6d03462d3024d80138394688b1add7abd
akbarpn136 <akbarpn136@gmail.com>
2017-03-10 18:57:47 +0700

    menambahkan fungsi untuk ambil anggota kegiatan beserta fungsi pagination

--- static/anotePage1/src/app/kegiatan-catatan/anggota-kegiatan/anggota-kegiatan.component.ts
+++ static/anotePage1/src/app/kegiatan-catatan/anggota-kegiatan/anggota-kegiatan.component.ts
@@ -16,26 +16,36 @@ export class AnggotaKegiatanComponent implements OnInit {
     anggota_count: any;
     key: any;
     messages: any;
+    offset: number = 0;
 
     constructor(private active: ActivatedRoute,
                 private anggota: AnggotaService) {
         this.active.parent.params.subscribe(val => {
             this.kegiatan_id = +val['id'];
-            this.anggota.ambilAnggota(this.kegiatan_id).subscribe(
-                (anggota) => {
-                    this.anggota_kegiatan = anggota['results'];
-                    this.anggota_next = anggota['next'];
-                    this.anggota_previous = anggota['previous'];
-                    this.anggota_count = anggota['count'];
-                },
-                (err) => {
-                    this.messages = err;
-                    this.key = Object.keys(this.messages);
-                }
-            );
+            this.ambilAnggota(this.kegiatan_id, this.offset);
         });
     }
 
     ngOnInit() {
+    }
+
+    onNewOffset(event) {
+        this.offset += +event;
+        this.ambilAnggota(this.kegiatan_id, this.offset);
+    }
+
+    ambilAnggota(id, offset) {
+        this.anggota.ambilAnggota(id, offset).subscribe(
+            (anggota) => {
+                this.anggota_kegiatan = anggota['results'];
+                this.anggota_next = anggota['next'];
+                this.anggota_previous = anggota['previous'];
+                this.anggota_count = anggota['count'];
+            },
+            (err) => {
+                this.messages = err;
+                this.key = Object.keys(this.messages);
+            }
+        );
     }
 }
