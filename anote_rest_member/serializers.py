2890c4ec22bb7556a975ffea05ae3ac7a30bd6c6
akbarpn136 <akbarpn136@gmail.com>
2017-03-10 11:42:01 +0700

    menambahkan field serializer untuk nama lengkap dan kegiatan

--- anote_rest_member/serializers.py
+++ anote_rest_member/serializers.py
@@ -4,12 +4,25 @@ from . import models
 
 
 class MemberSerializer(serializers.ModelSerializer):
+    personil_fullname = serializers.SerializerMethodField('get_user_fullname')
+    nama_kegiatan = serializers.SerializerMethodField('ambil_nama_kegiatan')
+
     class Meta:
         model = models.MemberKegiatan
         fields = (
             'id',
             'kegiatan',
+            'nama_kegiatan',
+            'personil_fullname',
             'personil',
             'jabatan',
             'kode_jabatan'
         )
+
+    @staticmethod
+    def get_user_fullname(obj):
+        return obj.get_user_fullname()
+
+    @staticmethod
+    def ambil_nama_kegiatan(obj):
+        return obj.get_nama_kegiatan()
