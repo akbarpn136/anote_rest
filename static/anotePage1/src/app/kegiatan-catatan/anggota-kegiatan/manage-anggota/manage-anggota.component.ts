e4167857bc71c2719f51dbbbf55fb73a73c00a83
akbarpn136 <akbarpn136@gmail.com>
2017-03-12 06:50:21 +0700

    tambah komponen baru untuk tambah/edit kegiatan

---
+++ static/anotePage1/src/app/kegiatan-catatan/anggota-kegiatan/manage-anggota/manage-anggota.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'an-manage-anggota',
+    templateUrl: './manage-anggota.component.html',
+    styleUrls: ['./manage-anggota.component.css']
+})
+export class ManageAnggotaComponent implements OnInit {
+    private isShow: boolean = true;
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
