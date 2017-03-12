44754426695ca1bc0e1ef4f4004c7e01d22019af
akbarpn136 <akbarpn136@gmail.com>
2017-03-12 14:38:58 +0700

    membuat service untuk tambah anggota

--- static/anotePage1/src/app/services/anggota.service.ts
+++ static/anotePage1/src/app/services/anggota.service.ts
@@ -37,6 +37,29 @@ export class AnggotaService {
             });
     }
 
+    tambahAnggota(kegiatan_id, data) {
+        this.qwerty = localStorage.getItem('qwerty');
+        let headers = new Headers({'Content-Type': 'application/json'});
+
+        headers.set('Authorization', `token ${this.qwerty}`);
+
+        let options = new RequestOptions({headers: headers});
+
+        return this.http.post(`${this.ANGGOTA_KEGIATAN_URL}${kegiatan_id}/anggota/`, data, options)
+            .map((res: Response) => {
+                return res.json();
+            })
+            .catch((err: Response | any) => {
+                if (err instanceof Response) {
+                    return Observable.throw(err.json());
+                }
+
+                else {
+                    return err.message;
+                }
+            });
+    }
+
     ambilPersonil(personil_id) {
         this.qwerty = localStorage.getItem('qwerty');
         let headers = new Headers({'Content-Type': 'application/json'});
