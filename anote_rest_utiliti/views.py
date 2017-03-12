0e4b4103198a84de04b85241dd6d015fbdffc725
akbarpn136 <akbarpn136@gmail.com>
2017-03-12 14:44:41 +0700

    penyesuaian objek json yang digunakan untuk user dan jabatan

--- anote_rest_utiliti/views.py
+++ anote_rest_utiliti/views.py
@@ -8,11 +8,18 @@ from anote_rest_member.models import OPSI_JABATAN
 # Create your views here.
 class GetUser(ListView):
     def get(self, request, *args, **kwargs):
-        user = {u.pk: u.get_full_name() for u in User.objects.all()}
+        user = [
+            {'pk': u.pk, 'nama': u.get_full_name()} for u in User.objects.all()
+        ]
+
         return JsonResponse(data=user, safe=False)
 
 
 class GetJabatan(ListView):
     def get(self, request, *args, **kwargs):
-        jabatan = {o[0]: o[1] for o in OPSI_JABATAN}
+        # jabatan = {o[0]: o[1] for o in OPSI_JABATAN}
+        jabatan = [
+            {'simbol': o[0], 'nama': o[1]} for o in OPSI_JABATAN
+        ]
+
         return JsonResponse(data=jabatan, safe=False)
