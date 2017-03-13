e6db688e00efbe554b23a77ec6bf35768105024b
akbarpn136 <akbarpn136@gmail.com>
2017-03-13 15:10:37 +0700

    menambahkan aplikasi route untuk crud anggota kegiatan

--- static/anotePage1/src/app/app.route.ts
+++ static/anotePage1/src/app/app.route.ts
@@ -8,6 +8,7 @@ import {DaftarCatatanComponent} from "./kegiatan-catatan/daftar-catatan/daftar-c
 import {CatatanComponent} from "./kegiatan-catatan/daftar-catatan/catatan.component";
 import {ModifKegiatanComponent} from "./kegiatan-catatan/daftar-kegiatan/modif-kegiatan.component";
 import {AnggotaKegiatanComponent} from "./kegiatan-catatan/anggota-kegiatan/anggota-kegiatan.component";
+import {ManageAnggotaComponent} from "./kegiatan-catatan/anggota-kegiatan/manage-anggota/manage-anggota.component";
 
 const APP_ROUTES: Route[] = [
     {path: 'masuk', component: LoginComponent, canActivate:[GuardAuthorizeService]},
@@ -15,6 +16,8 @@ const APP_ROUTES: Route[] = [
         {path: ':id/modifikasi', component: ModifKegiatanComponent},
         {path: ':id', component: DaftarCatatanComponent, children: [
             {path: 'anggota', component: AnggotaKegiatanComponent},
+            {path: 'anggota/tambah', component: ManageAnggotaComponent},
+            {path: 'anggota/:id/ubah', component: ManageAnggotaComponent},
         ]},
     ]},
     {path: '', redirectTo:'kegiatan', pathMatch:'full'},
